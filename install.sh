66fed9350f8d3acfd255c47261b8171e379aab42
Greg Wuller <greg@afofo.com>
2015-04-12 21:59:46 -0700

    Fixed up install.sh for renamed modules

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #! /bin/sh
 
-mkdir $1/app
-mkdir $1/mod
-mkdir $1/data
-mkdir $1/data/bees
-mkdir $1/data/bees/scalers
-mkdir $1/data/bees/scenes/
+mkdir -pv $1/app
+mkdir -pv $1/mod
+mkdir -pv $1/data
+mkdir -pv $1/data/bees
+mkdir -pv $1/data/bees/scalers
+mkdir -pv $1/data/bees/scenes
 
-cp utils/param_scaling/scaler_*.dat $1/data/bees/scalers/
-cp apps/bees/aleph-bees*.hex $1/app/
-cp modules/waves/aleph-waves.ldr $1/mod/
-cp modules/waves/aleph-waves.dsc $1/mod/
-cp modules/lines/aleph-lines.ldr $1/mod/
-cp modules/lines/aleph-lines.dsc $1/mod/
+cp -v utils/param_scaling/scaler_*.dat $1/data/bees/scalers/
+cp -v apps/bees/aleph-bees*.hex $1/app/
+cp -v modules/waves/aleph-waves.ldr $1/mod/
+cp -v modules/waves/aleph-waves.dsc $1/mod/
+cp -v modules/lines/aleph-lines.ldr $1/mod/
+cp -v modules/lines/aleph-lines.dsc $1/mod/
+cp -v modules/dsyn/aleph-dsyn.ldr $1/mod/
+cp -v modules/dsyn/aleph-dsyn.dsc $1/mod/
