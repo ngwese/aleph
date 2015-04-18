74de28eaf8d101f3fb554d1fb64228d361130e11
Greg Wuller <greg@afofo.com>
2015-04-17 22:47:04 -0700

    WIP: more frames, crash on key

--- apps/bees/src/ops/op_frames.c
+++ apps/bees/src/ops/op_frames.c
@@ -1,5 +1,16 @@
+
+// asf
+#include "print_funcs.h"
+
 #include "net_protected.h"
 #include "op_frames.h"
+
+#define CHAN_A 0
+#define CHAN_B 1
+#define CHAN_C 2
+#define CHAN_D 3
+
+#define INDEX_MAX (OP_FRAMES_LENGTH - 1)
 
 //-------------------------------------------------
 //---- static func declare
@@ -10,6 +21,7 @@ static void op_frames_in_d(op_frames_t* frames, const io_t v);
 static void op_frames_in_step(op_frames_t* frames, const io_t v);
 static void op_frames_in_index(op_frames_t* frames, const io_t v);
 static void op_frames_in_key(op_frames_t* frames, const io_t v);
+static void op_frames_in_clear(op_frames_t* frames, const io_t v);
 static void op_frames_in_ma(op_frames_t* frames, const io_t v);
 static void op_frames_in_mb(op_frames_t* frames, const io_t v);
 static void op_frames_in_mc(op_frames_t* frames, const io_t v);
@@ -19,16 +31,22 @@ static void op_frames_in_md(op_frames_t* frames, const io_t v);
 static u8* op_frames_pickle(op_frames_t* op, u8* dst);
 static const u8* op_frames_unpickle(op_frames_t* op, const u8* src);
 
+// helpers
+static void maybe_key(op_frames_t* frames, const io_t v, const u8 chan);
+static void do_clear(op_frames_t* frames, const io_t v);
+static void do_mute(op_frames_t* frames, const io_t v, const u8 chan);
+
 //-------------------------------------------------
 //---- static vars
-static const char* op_frames_instring = "INDEX\0  STEP\0   KEY\0    A\0      B\0      C\0      D\0      MUTEA\0  MUTEB\0  MUTEC\0  MUTED\0";
+static const char* op_frames_instring = "INDEX\0  STEP\0   KEY\0    CLEAR\0  A\0      B\0      C\0      D\0      MUTEA\0  MUTEB\0  MUTEC\0  MUTED\0";
 static const char* op_frames_outstring = "A\0      B\0      C\0      D\0      ";
 static const char* op_frames_opstring = "FRAMES";
 
-static op_in_fn op_frames_in_fn[11] = {
+static op_in_fn op_frames_in_fn[12] = {
   (op_in_fn)&op_frames_in_index,
   (op_in_fn)&op_frames_in_step,
   (op_in_fn)&op_frames_in_key,
+  (op_in_fn)&op_frames_in_clear,
   (op_in_fn)&op_frames_in_a,
   (op_in_fn)&op_frames_in_b, 
   (op_in_fn)&op_frames_in_c,
@@ -44,9 +62,12 @@ static op_in_fn op_frames_in_fn[11] = {
 void op_frames_init(void* mem) {
   u8 c;
   op_frames_t* frames = (op_frames_t*)mem;
-  frames->super.numInputs = 11;
+  frames->super.numInputs = 12;
   frames->super.numOutputs = 4;
-  frames->outs[0] = -1;
+
+  for (c = 0; c < OP_FRAMES_SIZE; c++) {
+    frames->outs[0] = 0;
+  }
 
   frames->super.in_fn = op_frames_in_fn;
   frames->super.in_val = frames->in_val;
@@ -62,18 +83,20 @@ void op_frames_init(void* mem) {
   frames->in_val[0] = &(frames->index);
   frames->in_val[1] = &(frames->step);
   frames->in_val[2] = &(frames->key);
-  frames->in_val[3] = &(frames->in[0]);
-  frames->in_val[4] = &(frames->in[1]);
-  frames->in_val[5] = &(frames->in[2]);
-  frames->in_val[6] = &(frames->in[3]);
-  frames->in_val[7] = &(frames->mute[0]);
-  frames->in_val[8] = &(frames->mute[1]);
-  frames->in_val[9] = &(frames->mute[2]);
-  frames->in_val[10] = &(frames->mute[3]);
+  frames->in_val[3] = &(frames->clear);
+  frames->in_val[4] = &(frames->in[0]);
+  frames->in_val[5] = &(frames->in[1]);
+  frames->in_val[6] = &(frames->in[2]);
+  frames->in_val[7] = &(frames->in[3]);
+  frames->in_val[8] = &(frames->mute[0]);
+  frames->in_val[9] = &(frames->mute[1]);
+  frames->in_val[10] = &(frames->mute[2]);
+  frames->in_val[11] = &(frames->mute[3]);
 
   frames->index = 0;
   frames->step = 0;
   frames->key = 0;
+  frames->clear = 0;
   for (c = 0; c < OP_FRAMES_SIZE; c++) {
     frames->in[c] = 0;
   }
@@ -81,62 +104,158 @@ void op_frames_init(void* mem) {
     frames->mute[c] = 0;
   }
 
-  // TODO: clear buffer?
+  do_clear(frames, 0);
 }
 
 //-------------------------------------------------
 //---- static func define
 
+static void maybe_key(op_frames_t* frames, const io_t v, const u8 chan) {
+  if (frames->key != 0) {
+    print_dbg("\r\n FRAMES: maybe_key");
+    frames->keys[frames->index][chan] = v;
+    print_dbg(" Ox");
+    print_dbg_hex(frames->index);
+    print_dbg(" Ox");
+    print_dbg_hex(chan);
+    print_dbg(" Ox");
+    print_dbg_hex(v);
+    net_activate(frames->outs[chan], v, frames);
+  }   
+}
+
+static void do_clear(op_frames_t* frames, const io_t v) {
+  print_dbg("\r\n FRAMES: do_clear");
+  for (u8 s = 0; s < OP_FRAMES_LENGTH; s++) {
+    for (u8 c = 0; c < OP_FRAMES_SIZE; c++) {
+      frames->keys[s][c] = v;
+    }
+  }
+  print_dbg(" ...done.");
+}
+
 static void op_frames_in_a(op_frames_t* frames, const io_t v) {
-  frames->in[0] = v;
+  frames->in[CHAN_A] = v;
+  maybe_key(frames, v, CHAN_A);
 }
 
 static void op_frames_in_b(op_frames_t* frames, const io_t v) {
-  frames->in[1] = v;
+  frames->in[CHAN_B] = v;
+  maybe_key(frames, v, CHAN_B);
 }
 
 static void op_frames_in_c(op_frames_t* frames, const io_t v) {
-  frames->in[2] = v;
+  frames->in[CHAN_C] = v;
+  maybe_key(frames, v, CHAN_C);
 }
 
 static void op_frames_in_d(op_frames_t* frames, const io_t v) {
-  frames->in[3] = v;
+  frames->in[CHAN_D] = v;
+  maybe_key(frames, v, CHAN_D);
+}
+
+static void do_mute(op_frames_t* frames, const io_t v, const u8 chan) {
+  if (v > 0) {
+    if (frames->mute[chan] == 0)
+      frames->mute[chan] = 1;
+  }
+  else {
+    if (frames->mute[chan] > 0)
+      frames->mute[chan] = 0;
+  }
 }
 
 static void op_frames_in_ma(op_frames_t* frames, const io_t v) {
-  frames->mute[0] = v;
+  do_mute(frames, v, CHAN_A);
 }
 
 static void op_frames_in_mb(op_frames_t* frames, const io_t v) {
-  frames->mute[1] = v;
+  do_mute(frames, v, CHAN_B);
 }
 
 static void op_frames_in_mc(op_frames_t* frames, const io_t v) {
-  frames->mute[2] = v;
+  do_mute(frames, v, CHAN_C);
 }
 
 static void op_frames_in_md(op_frames_t* frames, const io_t v) {
-  frames->mute[3] = v;
+  do_mute(frames, v, CHAN_D);
 }
 
 static void op_frames_in_key(op_frames_t* frames, const io_t v) {
-  // store abcd values at index
-  u8 i = frames->index % OP_FRAMES_LENGTH;
-  for (u8 c = 0; c < OP_FRAMES_SIZE; c++) {
-    frames->keys[i][c] = frames->in[c];
+  print_dbg("\r\n FRAMES: op_frames_in_key");
+  if (v > 0) {
+    if (frames->key == 0) {
+      frames->key = 1;
+      print_dbg(" at: 0x");
+      print_dbg_hex(frames->index);
+      // store abcd values at index
+      for (u8 c = 0; c < OP_FRAMES_SIZE; c++) {
+      	//frames->keys[frames->index][c] = frames->in[c];
+        print_dbg(" 0x");
+        print_dbg_hex(frames->in[c]);
+      }
+    }
+  }
+  else {
+    if (frames->key > 0)
+      frames->key = 0;
   }
 }			      
 
 static void op_frames_in_step(op_frames_t* frames, const io_t v) {
+  if (v < -INDEX_MAX) {
+    frames->step = -INDEX_MAX;
+  }
+  else if (v > INDEX_MAX) {
+    frames->step = INDEX_MAX;
+  }
+  else {
+    frames->step = v;
+  }
+  
   // move to next index based on step
-  io_t next_idx = (frames->index + v) % OP_FRAMES_LENGTH;
+  io_t next_idx = (frames->index + frames->step) % OP_FRAMES_LENGTH;
+  if (next_idx < 0) {
+    next_idx = INDEX_MAX + next_idx;
+  }
+  print_dbg("\r\n FRAMES: op_frames_in_step, next: 0x");
+  print_dbg_hex(next_idx);
   op_frames_in_index(frames, next_idx);
 }
 
 static void op_frames_in_index(op_frames_t* frames, const io_t v) {
-  frames->index = frames->index % OP_FRAMES_LENGTH;
+  if (v > INDEX_MAX) {
+    frames->index = INDEX_MAX;
+  }
+  else if (v < 0) {
+    frames->index = 0;
+  }
+  else {
+    frames->index = v;
+  }
+
+  print_dbg("\r\n FRAMES: op_frames_in_index, i: 0x");
+  print_dbg_hex(frames->index);
+
   for (u8 c = 0; c < OP_FRAMES_SIZE; c++) {
+    print_dbg(" 0x");
+    print_dbg_hex(frames->keys[frames->index][c]);
     net_activate(frames->outs[c], frames->keys[frames->index][c], frames);
+  }
+}
+
+static void op_frames_in_clear(op_frames_t* frames, const io_t v) {
+  if (v > 0) {
+    if (frames->clear == 0)
+      frames->clear = 1;
+  }
+  else {
+    if (frames->clear > 0)
+      frames->clear = 0;
+  }
+
+  if (frames->clear) {
+    do_clear(frames, 0);
   }
 }
 
@@ -154,7 +273,13 @@ u8* op_frames_pickle(op_frames_t* op, u8* dst) {
   dst = pickle_io(op->mute[2], dst);
   dst = pickle_io(op->mute[3], dst);
 
-  // TODO: store buffer
+  // store keys
+  for (u8 s = 0; s < OP_FRAMES_LENGTH; s++) {
+    for (u8 c = 0; c < OP_FRAMES_SIZE; c++) {
+      dst = pickle_io(op->keys[s][c], dst);
+    }
+  }
+
   return dst;
 }
 
@@ -169,6 +294,12 @@ const u8* op_frames_unpickle(op_frames_t* op, const u8* src) {
   src = unpickle_io(src, &(op->mute[2]));
   src = unpickle_io(src, &(op->mute[3]));
 
-  // TODO: recall buffer
+  // recall keys
+  for (u8 s = 0; s < OP_FRAMES_LENGTH; s++) {
+    for (u8 c = 0; c < OP_FRAMES_SIZE; c++) {
+      src = unpickle_io(src, &(op->keys[s][c]));
+    }
+  }
+
   return src;
 }
--- apps/bees/src/ops/op_frames.h
+++ apps/bees/src/ops/op_frames.h
@@ -14,9 +14,10 @@ typedef struct op_frames_struct {
   volatile io_t step;
   volatile io_t index;
   volatile io_t key;
+  volatile io_t clear;
   volatile io_t in[OP_FRAMES_SIZE];
   volatile io_t mute[OP_FRAMES_SIZE];
-  volatile io_t * in_val[11]; // step, index, trig, key, mute
+  volatile io_t * in_val[12]; // step, index, trig, key, mute
   op_out_t outs[4]; // a, b, c, d
 
   io_t keys[OP_FRAMES_LENGTH][OP_FRAMES_SIZE];
